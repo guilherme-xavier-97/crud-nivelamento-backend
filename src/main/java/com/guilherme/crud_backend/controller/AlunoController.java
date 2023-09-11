41dc6d6ab1502afa238a9911c6bd746325ad7855
Otávio Guilherme Xavier da Silva <otavio.guilherme@unesp.br>
2023-09-11 10:15:27 -0300

    Inclusao do docker compose e configuração dos ambientes test (com db h2) e dev (com db postgres via imagem docker)

--- src/main/java/com/guilherme/crud_backend/controller/AlunoController.java
+++ src/main/java/com/guilherme/crud_backend/controller/AlunoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,7 +19,8 @@ import com.guilherme.crud_backend.model.Aluno;
 import com.guilherme.crud_backend.service.AlunoService;
 
 @RestController
-@RequestMapping("/alunos")
+@RequestMapping("/api/alunos")
+@CrossOrigin
 public class AlunoController {
 
     @Autowired
